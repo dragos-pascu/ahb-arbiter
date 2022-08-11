abbc37592fb9bcccfdc6ea767208fb1953430ae8
Dragos Pascu <dragos.pascu@dragos.pascu>
2022-08-11 15:54:37 +0300

    Changed some sequence names

--- agent/ahb_seq/ahb_base_seq.sv
+++ agent/ahb_seq/ahb_base_seq.sv
@@ -4,25 +4,19 @@ class ahb_base_seq extends uvm_sequence #(ahb_transaction);
 
     //see uvm docs, uvm_sequence has a builtin handle req and rsp of sequence item type
 
-    function new(string name = "ahb_seq");
+    function new(string name = "ahb_base_seq");
         super.new(name);
     endfunction
 
 
-    // virtual task body();
-    //     `uvm_info(get_type_name(),"Call ahb_seq", UVM_LOW)
-    //     repeat(5) begin
-    //     `uvm_do(req)
-    //     end
-    // endtask
-
     virtual task body();
-        `uvm_info(get_type_name(),"Starting body of ahb_base_seq", UVM_MEDIUM)
-        start_item(req);
-        req.randomize();
-        finish_item(req);
-        get_response(rsp);
+        `uvm_info(get_type_name(),"Call ahb_base_seq", UVM_LOW)
+        repeat(5) begin
+        `uvm_do(req)
+        end
     endtask
+
+
 
     
 endclass
