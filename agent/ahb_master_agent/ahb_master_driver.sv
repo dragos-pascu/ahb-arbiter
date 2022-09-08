e362d8a1473ac5b6af14cea2f9bf9864a03bb6d3
Dragos Pascu <dragos.pascu@dragos.pascu>
2022-09-08 14:27:06 +0300

    driver changes for correct incr busreq

--- agent/ahb_master_agent/ahb_master_driver.sv
+++ agent/ahb_master_agent/ahb_master_driver.sv
@@ -8,7 +8,6 @@ class ahb_master_driver extends uvm_driver#(ahb_transaction);
 
     mailbox mbx = new();
 
-    int flag= 0;
 
     function new(string name = "ahb_master_driver", uvm_component parent);
         super.new(name, parent);
@@ -57,23 +56,15 @@ class ahb_master_driver extends uvm_driver#(ahb_transaction);
         forever begin                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
             seq_item_port.get(req);
             req.print();
-            flag=0;
             
             vif.m_cb.hbusreq <= 1;
             vif.m_cb.hlock <= 1;
             
             //drive address
 
-            
-            
             foreach (req.haddr[i]) begin
                 
-
-                if (i == req.haddr.size() - 1) begin
-                    @(vif.m_cb iff(vif.m_cb.hready));
-                    vif.m_cb.hbusreq <= 0;  
-                    vif.m_cb.hlock <= 0;
-                end
+                
 
                 vif.m_cb.haddr  <= req.haddr[i];
                 vif.m_cb.htrans <= req.htrans[i];
@@ -84,30 +75,44 @@ class ahb_master_driver extends uvm_driver#(ahb_transaction);
                 vif.m_cb.hburst  <= req.hburst;
                 
                 
-                #1;
+                
                 //wait(vif.m_cb.hgrant & vif.m_cb.hready); expresia se executa in timp 0 daca expresia este true
                 if (i != req.haddr.size()-1) begin
-                    @(vif.m_cb iff(vif.m_cb.hgrant & vif.m_cb.hready));
-                    if(flag==0) begin
-                    mbx.put(req);
-                    flag=1;
-                    end
-                    
+                    @(vif.m_cb iff(vif.m_cb.hgrant & vif.m_cb.hready));                    
                 end
-
-                
+                else begin
+                    if (vif.m_cb.hready==1) begin
+                        vif.m_cb.hbusreq <= 0;  
+                        vif.m_cb.hlock <= 0;
+                    end
+                end
+                // @(vif.m_cb iff(vif.m_cb.hgrant & vif.m_cb.hready));  
+                // if (i == req.haddr.size()-1) begin
+                //       if (vif.m_cb.hready==1) begin
+                //         vif.m_cb.hbusreq <= 0;  
+                //         vif.m_cb.hlock <= 0;
+                //     end                
+                // end
+                              
+                if (i==0) begin
+                    mbx.put(req);
+                end
+                // if (i == req.haddr.size()-1) begin
+                //     @(vif.m_cb iff(vif.m_cb.hready == 1));
+                    
+                // end
                 
 
             end
 
-            
+
             //#1; este in for each de la ultima iteratie
             
-            //@(vif.m_cb iff(vif.m_cb.hready == 1));
-            //vif.m_cb.htrans <= IDLE;
+            // @(vif.m_cb iff(vif.m_cb.hready == 1));
+            // vif.m_cb.htrans <= 0;
             // vif.m_cb.hbusreq <= 0;  
             // vif.m_cb.hlock <= 0;
-            
+            //vif.m_cb.htrans <= 0;
         end
     endtask
 
@@ -120,13 +125,16 @@ class ahb_master_driver extends uvm_driver#(ahb_transaction);
             //drive data items
             foreach (item.haddr[i]) begin
             
-                @(vif.m_cb iff(vif.m_cb.hready == 1));
-                if(item.hwrite == WRITE)
-                begin
+                //@(vif.m_cb iff(vif.m_cb.hready == 1));
+                if (vif.m_cb.hready == 1) begin
+                    if(item.hwrite == WRITE)
+                    begin
 
                     vif.m_cb.hwdata <= item.hwdata[i];
-                    
+                    @vif.m_cb;
+                    end
                 end
+                
             end
             seq_item_port.put(item);
         end
--- agent/ahb_seq/sequence_lib.sv
+++ agent/ahb_seq/sequence_lib.sv
@@ -12,7 +12,7 @@ class simple_write_sequence extends uvm_sequence#(ahb_transaction);
         `uvm_info(get_type_name(),"Inside body of simple_write_sequence.",UVM_MEDIUM)
 
         req = ahb_transaction::type_id::create("req");
-        repeat(2)begin
+        repeat(1)begin
         start_item(req);
         // if(!req.randomize())
         //     `uvm_fatal(get_type_name(), "Single write randomize failed!")
