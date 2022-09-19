67e0aa544203801b05bcfef3cc325374368bfeea
Dragos Pascu <dragos.pascu@dragos.pascu>
2022-09-19 16:07:39 +0300

    file was not added for commit

--- agent/ahb_coverage.sv
+++ agent/ahb_coverage.sv
@@ -1,5 +1,5 @@
-class ahb_coverage extends uvm_subscriber#(ahb_transaction);
-    `uvm_component_utils(ahb_coverage)
+class ahb_master_coverage extends uvm_subscriber#(ahb_transaction);
+    `uvm_component_utils(ahb_master_coverage)
 
     ahb_transaction item;
 
@@ -13,14 +13,20 @@ class ahb_coverage extends uvm_subscriber#(ahb_transaction);
         hbusreq: coverpoint item.hbusreq;
         hlock: coverpoint item.hlock;
 
-        read_write: coverpoint item.hwrite;
+        read_write: coverpoint item.hwrite {bins read_write_bin = {WRITE};}
         htrans: coverpoint item.htrans[0];
-        haddr : coverpoint item.haddr[0] {option.auto_bin_max = 32;}
+        haddr : coverpoint item.haddr[0] {
+            bins range_0  = {['d0:'d69]};
+            bins range_1  = {['d70:'d140]};
+            bins range_2  = {['d141:'d210]};
+            bins range_3  = {['d211:'d281]};
+            bins range_4  = {['d282:'d350]};
+        }
         hburst : coverpoint item.hburst;
         hsize: coverpoint item.hsize {bins word_bin = {WORD};}
 
-        hwdata: coverpoint item.hwdata[0] {option.auto_bin_max = 32;}
-        hrdata: coverpoint item.hrdata {option.auto_bin_max = 32;}
+        hwdata: coverpoint item.hwdata[0] {option.auto_bin_max = 6;}
+        hrdata: coverpoint item.hrdata {option.auto_bin_max = 6;}
 
         hready: coverpoint item.hready;
         hresp: coverpoint item.hresp {bins rsp = {OKAY, ERROR};}
@@ -50,7 +56,7 @@ class ahb_coverage extends uvm_subscriber#(ahb_transaction);
     endfunction
 
     function void report_phase(uvm_phase phase);
-                `uvm_info(get_type_name(), $sformatf("Coverage is: %f", ahb_cg.get_coverage()), UVM_MEDIUM)
+                `uvm_info(get_type_name(), $sformatf("Coverage for master interface is: %f", ahb_cg.get_coverage()), UVM_MEDIUM)
                 
                 // `uvm_info(get_type_name(), $sformatf("wdata coverage is: %f", ahb_cg.hwdata.get_coverage()), UVM_MEDIUM)
                 // `uvm_info(get_type_name(), $sformatf("hresp coverage is: %f", ahb_cg.hresp.get_coverage()), UVM_MEDIUM)
