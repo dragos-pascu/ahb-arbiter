043973f428f757e70d25cbc4e864243809f9365e
dragos <dragos.pascu@tremend.com>
2023-03-24 09:15:34 +0200

    .

--- sim/run.f
+++ sim/run.f
@@ -29,11 +29,11 @@
 //+UVM_TESTNAME=incr_write_test
 //+UVM_TESTNAME=incr_read_test
 
-//+UVM_TESTNAME=incr_write_4_test
++UVM_TESTNAME=incr_write_4_test
 //+UVM_TESTNAME=incr_write_8_test
 //+UVM_TESTNAME=incr_write_16_test
 
-+UVM_TESTNAME=incr_read_4_test
+//+UVM_TESTNAME=incr_read_4_test
 //+UVM_TESTNAME=incr_read_8_test
 //+UVM_TESTNAME=incr_read_16_test
 
