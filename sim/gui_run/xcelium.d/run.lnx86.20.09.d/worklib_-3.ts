0197d5d4a54d6478ff8a9268c2f82b2255632d07
dragos <dragos.pascu@tremend.com>
2023-03-24 09:12:05 +0200

    deleted files that should've been to .gitignore

--- sim/gui_run/xcelium.d/run.lnx86.20.09.d/worklib_-3.ts
+++
@@ -1,2 +0,0 @@
-1526655669 /sw/cadence/XCELIUM2009/tools.lnx86/methodology/UVM/CDNS-1.1d/sv/src/uvm_pkg.sv
-1526655664 /sw/cadence/XCELIUM2009/tools.lnx86/methodology/UVM/CDNS-1.1d/additions/sv/cdns_uvm_pkg.sv
